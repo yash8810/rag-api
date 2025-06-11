221455aedb4b5f41a0afd019c8c8ce86e90e4c5e
GymerMarketPlace <yash8810patel@gmail.com>
2025-06-11 08:48:17 +0530

    update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ print("Starting Flask app...")
 
 @app.route('/', methods=['GET'])
 def hello():
-    return "Hello world to me"
+    return "Hi"
 
 @app.route('/api/search', methods=['POST'])
 def search():
